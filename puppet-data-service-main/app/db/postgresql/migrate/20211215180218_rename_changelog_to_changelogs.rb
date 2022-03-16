a89a3fb8e49c50b21ac5e66f2fbfd57cd436cd8c
Reid Vandewiele <reid@puppet.com>
2022-03-16 16:17:15 -0700

    Set contents to puppetlabs-peadm-3.4.0.tar.gz

--- puppet-data-service-main/app/db/postgresql/migrate/20211215180218_rename_changelog_to_changelogs.rb
+++
@@ -1,7 +0,0 @@
-class RenameChangelogToChangelogs < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :changelog, :username
-    rename_table :changelog, :changelogs
-    add_index :changelogs, :username
-  end
-end
--- puppet-data-service-main/golang/pkg/pds-go-client/doc.go
+++
@@ -1,3 +0,0 @@
-package client
-
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml ../../../docs/api.yml
